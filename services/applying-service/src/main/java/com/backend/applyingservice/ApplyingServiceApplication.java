9b465b478b88876e3f654669d3a1de2125b49b04
ThanhHai2k3 <kuriyamamirai1162k3@gmail.com>
2025-12-25 17:36:54 +0700

    create applying-service: resolve application processing logic

---
+++ services/applying-service/src/main/java/com/backend/applyingservice/ApplyingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.applyingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyingServiceApplication.class, args);
+	}
+
+}
---
+++ services/applying-service/src/test/java/com/backend/applyingservice/ApplyingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.applyingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
